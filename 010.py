f0f89199e1a381935a4a7af5cef48b9924dc4efc
Akim <akimkom@protonmail.com>
2022-07-02 18:03:18 +0100

    Solved 010

---
+++ 010.py
@@ -0,0 +1,57 @@
+import math
+import logging
+
+# From problem #7.
+
+def input_valid_num():
+    num_of_p = -1
+    valid = False
+    while valid is False:
+        try:
+            num_of_p = int(input("Enter the number of primes you want to know: "))
+            valid = True
+            if num_of_p < 0: 
+                print("The number should be natural!")
+                valid = False
+        except ValueError:
+            print("n is not an integer!")
+    return num_of_p
+
+
+def is_prime(num):
+    check_before = int(math.sqrt(num))
+    for factor in primes:
+        if factor >= check_before + 1:
+            return True
+        if num % factor == 0:
+            return False
+    return True
+
+
+def find_primes(ceiling):
+    global primes
+    primes = []
+    cur = 2
+    while cur <= ceiling:
+        if is_prime(cur):
+            primes.append(cur)
+        cur += 1
+
+
+
+def main():
+    ceiling = input_valid_num()
+    find_primes(ceiling)
+    sum = 0
+    for prime in primes:
+        sum += prime
+    print(sum)
+
+
+if __name__ == '__main__':
+    # Logging
+    lvl = logging.DEBUG 
+    fmt = '%(lineno)s: [%(levelname)s] %(msg)s'
+    logging.basicConfig(level = lvl, format = fmt)
+
+    main()
